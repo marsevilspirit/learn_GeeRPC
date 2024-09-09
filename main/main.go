da1cc264531e8d0732b5aba42bd9473c609a2dca
marsevilspirit <marsevilspirit@gmail.com>
2024-09-09 20:34:18 +0800

    add service

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"geerpc"
 	"log"
 	"net"
@@ -9,7 +8,21 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := geerpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
@@ -27,18 +40,18 @@ func main() {
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
+	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
-			if err := client.Call("Foo.Bar", args, &reply); err != nil {
-				log.Fatal("call Foo.Bar error:", err)
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
