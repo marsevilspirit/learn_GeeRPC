49d60c2c7ca067404c2cfbe788aea5355fbb35be
marsevilspirit <marsevilspirit@gmail.com>
2024-09-09 08:28:59 +0800

    add client

---
+++ codec/codec.go
@@ -0,0 +1,34 @@
+package codec
+
+import (
+	"io"
+)
+
+type Header struct {
+	ServiceMethod string // 服务名和方法名
+	Seq			  uint64 // 请求序号
+	Error         string // 错误信息
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+
+type Type string
+
+const (
+	GobType Type = "application/gob"
+	JsonType Type = "application/json" // not implemented
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
